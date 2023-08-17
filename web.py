49a5c92ba63479de54f95299ba4a07bcf2aaff3b
Deon Jordaan <deonhjordaan@gmail.com>
2023-08-17 19:53:47 +0200

    remove line

--- web.py
+++ web.py
@@ -24,5 +24,3 @@ for index, todo in enumerate(todos):
         st.experimental_rerun()
 
 st.text_input(label="Add a new todo.", placeholder="Add a new todo...", on_change=add_todo, key='new_todo')
-
-st.session_state
